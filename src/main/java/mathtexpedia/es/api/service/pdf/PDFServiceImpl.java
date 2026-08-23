e21b5066be0b4c697e9a55ea297d16cdaeb55ab3
Pablo García Pernas <160616028+PabloGarPe@users.noreply.github.com>
2026-08-23 22:21:09 +0200

    feat(pdf): Añade listado de pdf con link (#17)

--- src/main/java/mathtexpedia/es/api/service/pdf/PDFServiceImpl.java
+++ src/main/java/mathtexpedia/es/api/service/pdf/PDFServiceImpl.java
@@ -2,13 +2,14 @@ package mathtexpedia.es.api.service.pdf;
 
 import mathtexpedia.es.api.persistence.pdf.PDF;
 import mathtexpedia.es.api.persistence.pdf.PDFDataService;
+import mathtexpedia.es.api.presentation.GenericController;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
-public class PDFServiceImpl implements PDFService {
+public class PDFServiceImpl extends GenericController implements PDFService {
 
     @Autowired
     private PDFDataService pdfDataService;
@@ -16,5 +17,10 @@ public class PDFServiceImpl implements PDFService {
     @Override
     public List<PDF>  getPDFWithoutLink() {
         return pdfDataService.getPDFWithNoLink();
+    }
+
+    @Override
+    public List<PDF> getPDFs() {
+        return pdfDataService.getPDFs();
     }
 }
